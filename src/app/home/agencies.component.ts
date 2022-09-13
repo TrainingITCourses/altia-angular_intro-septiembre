ec5de8ec9768614b0df02e4bcdf1c4fe0628b4ca
Alberto Basalo <albertobasalo@hotmail.com>
2022-09-13 12:48:04 +0200

    feat: module home and components

---
+++ src/app/home/agencies.component.ts
@@ -0,0 +1,36 @@
+import { Component } from "@angular/core";
+import { data } from "../data.repository";
+@Component({
+  selector: "app-agencies",
+  template: `
+    <article>
+      <h3>We work with {{ getAgenciesCount() }} agencies</h3>
+      <ul>
+        <li *ngFor="let agency of agencies">
+          <span [class]="getClassByStatus(agency.status)">
+            {{ agency.name }}
+          </span>
+          <span *ngIf="agency.range === 'Interplanetary'">🪐</span>
+          <span *ngIf="agency.range === 'Orbital'">🌍</span>
+        </li>
+      </ul>
+    </article>
+  `,
+  styles: [
+    `
+      .active {
+        font-style: normal;
+        font-weight: bold;
+      }
+      .pending {
+        font-style: italic;
+      }
+    `,
+  ],
+})
+export class AgenciesComponent {
+  agencies = data.agencies;
+
+  getAgenciesCount = () => this.agencies.length;
+  getClassByStatus = (agencyStatus: string) => agencyStatus.toLowerCase();
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { AgenciesComponent } from "./agencies.component";
 import { HomeComponent } from "./home.component";
+import { TripsComponent } from './trips.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AgenciesComponent, TripsComponent],
   imports: [CommonModule],
   exports: [HomeComponent],
 })
