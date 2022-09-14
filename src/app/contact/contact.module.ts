3e5ebb21dc725e6c285ad655eeeb55292efc52e5
Alberto Basalo <albertobasalo@hotmail.com>
2022-09-14 13:32:10 +0200

    feat: minimal contact form

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
 import { ContactPage } from "./contact.page";
 
 @NgModule({
   declarations: [ContactPage],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: "",
--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-contact",
   template: `
     <h1>📧 Get in contact</h1>
     <p>Send us a message</p>
+    <form [formGroup]="contactForm">
+      <div>
+        <label for="email">Your email, please</label>
+        <input id="email" type="email" name="email" formControlName="email" />
+      </div>
+      <button (click)="onSave()">Send</button>
+    </form>
   `,
   styles: [],
 })
-export class ContactPage implements OnInit {
-  constructor() {}
+export class ContactPage {
+  contactForm: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(formBuilder: FormBuilder) {
+    this.contactForm = formBuilder.group({
+      email: "",
+    });
+  }
+
+  onSave() {
+    console.log(this.contactForm.value);
+  }
 }
